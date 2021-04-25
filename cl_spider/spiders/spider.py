3c0baaf62070bef4725989a44e8f60aebf31abf0
tengshan yanghe <yanghetengshan@bobfintech.com.cn>
2021-04-25 17:09:55 +0800

    UPDATE spider base

--- cl_spider/spiders/spider.py
+++ cl_spider/spiders/spider.py
@@ -1,9 +1,10 @@
 from typing import Dict, Text
-from mechanicalsoup import StatefulBrowser as Browser
+import urllib.parse
 import requests
-from loguru import logger
 from bs4 import BeautifulSoup
 from fake_useragent import FakeUserAgent
+from loguru import logger
+from mechanicalsoup import StatefulBrowser as Browser
 
 REQUESTS_EXCEPTION = (
     requests.exceptions.ReadTimeout,
@@ -14,40 +15,49 @@ REQUESTS_EXCEPTION = (
 TIMEOUT = (10, 30)
 
 
-def format_url(url):
-    pass
-
 
 class Spider:
     def __init__(self, headers: Dict[Text, Text] = None) -> None:
+        ua = FakeUserAgent()
+        # ua.update()
         if headers is None:
             self.headers = {
-                'User-Agent': FakeUserAgent().data_randomize
+                'User-Agent': ua.random
             }
         else:
             self.headers = headers
 
-    @staticmethod
-    def open(url: Text) -> BeautifulSoup:
-        with Browser() as browser:
+    def open(self, url: Text) -> BeautifulSoup:
+        with Browser(user_agent=self.headers['User-Agent']) as browser:
             soup: BeautifulSoup = None
             try:
                 browser.open(url, timeout=TIMEOUT)
             except REQUESTS_EXCEPTION as e:
-                logger.error(f"url is '{format_url(url)}', error is {e}.")
+                logger.error(f"route is '{self.format_url(url)}', error is {e}.")
             except Exception as e:
-                logger.error(f"url is '{format_url(url)}', error is {e}.")
+                logger.error(f"route is '{self.format_url(url)}', error is {e}.")
             else:
                 soup = browser.get_current_page()
 
             if soup is None:
-                logger.warning(f"url is {format_url(url)}, request failed.")
+                logger.warning(f"route is {self.format_url(url)}, request failed.")
                 return None
             if '無法找到頁面' in soup.head.title.string:
-                logger.warning(f"url is {format_url(url)}, page 404.")
+                logger.warning(f"route is {self.format_url(url)}, page 404.")
                 return None
 
             return soup
+
+    @staticmethod
+    def format_url(url: Text) -> Text:
+        r = urllib.parse.urlsplit(url)
+        if r.path and r.query and r.fragment:
+            return r.path + r.query + r.fragment
+        if r.path and r.query:
+            return r.path + r.query
+        if r.path:
+            return r.path
+        return url
 
     @staticmethod
     def format_string(text: Text) -> Text:
