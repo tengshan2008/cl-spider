d8fb7ba8580158d08c9fca5d079e7e991b7b1aba
tengshan yanghe <yanghetengshan@bobfintech.com.cn>
2021-04-25 17:08:43 +0800

    UPDATE picture/parse_data

--- cl_spider/spiders/picture.py
+++ cl_spider/spiders/picture.py
@@ -1,12 +1,12 @@
-import time
 import random
-from typing import Any, Dict, Optional, Text, List
+import time
+from typing import Any, Dict, List, Optional, Text
 
 from bs4 import BeautifulSoup
-
-from cl_spider.spiders.spider import Spider, format_url
 from cl_spider.app.models import Picture
 from cl_spider.spiders.manager import Manager
+from cl_spider.spiders.spider import Spider
+from loguru import logger
 
 WAIT_TIME_MIN = 1
 WAIT_TIME_MAX = 5
@@ -34,13 +34,19 @@ class PictureSpider(Spider):
     def load_data(self, url: Text) -> BeautifulSoup:
         return self.open(url)
 
-    def parse_title(self, url: Text, title: Text, length_of_imgs: int) -> Text:
+    def parse_title(self, url: Text, data: BeautifulSoup, length_of_imgs: int) -> Text:
+        title = data.head.title.string.strip()
+
         tid = url.split('/')[-1][:-5]
         title = self.format_string(title)
         title = title.replace("技術討論區草榴社區", "")
+
+        if f"[{length_of_imgs}P]" in title:
+            title = title.replace(f"[{length_of_imgs}P]", "")
         return f"{title}_[{length_of_imgs}P]_{tid}"
 
-    def parse_imgs(self, imgs: List[Any]):
+    def parse_imgs(self, data: BeautifulSoup):
+        imgs = data.body.find_all('img')
         for i, img in enumerate(imgs):
             link = ""
             if "data-src" in img.attrs:
@@ -63,12 +69,10 @@ class PictureSpider(Spider):
     def parse_data(self, url: Text, data: BeautifulSoup) -> Dict:
         parsed_data = {}
 
-        imgs = data.body.find_all('img')
-        title = data.head.title.string.strip()
+        parsed_data['imgs'] = list(self.parse_imgs(data))
+        parsed_data['title'] = self.parse_title(url, data, len(parsed_data['imgs']))
 
-        parsed_data['title'] = self.parse_title(url, title, len(imgs))
-        parsed_data['imgs'] = list(self.parse_imgs(imgs))
-
+        logger.info(f"route is '{self.format_url(url)}', have parsed.")
         return parsed_data
 
     def save_data(self, parse_data: Dict) -> None:
