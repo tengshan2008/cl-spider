7d3f9b0cdc44e07685846f2a5c25e447ca132cdd
tengshan2008 <tengshan2008@hotmail.com>
2021-05-04 14:36:54 +0800

    UPDATE server

--- server.py
+++ server.py
@@ -1,8 +1,6 @@
 from cl_spider.app import app
 from cl_spider.app.main import init_db
 
-app.debug = True
-
 if __name__ == "__main__":
     init_db()
-    app.run()
+    app.run(debug=False, use_reloader=False)
