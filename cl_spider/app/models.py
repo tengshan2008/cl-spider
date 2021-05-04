08d15433741b0c69dbd76d11a913ff46b7a6d9d4
tengshan2008 <tengshan2008@hotmail.com>
2021-05-04 14:39:22 +0800

    UPDATE models

--- cl_spider/app/models.py
+++ cl_spider/app/models.py
@@ -16,26 +16,32 @@ class Novel(db.Model):
     status = db.Column(db.Integer, default=0)
     path = db.Column(db.String(255), nullable=True)
     share = db.Column(db.String(255), nullable=True)
-    created_at = db.Column(db.DateTime, default=datetime.now())
-    updated_at = db.Column(db.DateTime, default=datetime.now())
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.now)
     deleted_at = db.Column(db.DateTime)
 
     def __unicode__(self):
-        return self.name
+        return self.title
 
     def __repr__(self):
-        return f"<Novel {self.name}>"
+        return f"<Novel {self.title}>"
 
 
 class Picture(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     origin_id = db.Column(db.String(255), default='UNKNOW')
     title = db.Column(db.String(255), default='UNKNOW')
-    authoer = db.Column(db.String(255), nullable=True)
+    author = db.Column(db.String(255), nullable=True)
     public_datetime = db.Column(db.DateTime, nullable=True)
+    link = db.Column(db.String(255), nullable=True)
+    share = db.Column(db.String(255), nullable=True)
+    status = db.Column(db.Integer, default=0)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.now)
+    deleted_at = db.Column(db.DateTime)
 
     def __unicode__(self):
-        return self.name
+        return self.title
 
     def __repr__(self):
-        return f"<Picture {self.name}>"
+        return f"<Picture {self.title}>"
