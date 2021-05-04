90347f2bb6e856c21bf242d12642c7a57e6c150c
tengshan2008 <tengshan2008@hotmail.com>
2021-05-04 14:39:02 +0800

    UPDATE Thread pool

--- cl_spider/app/__init__.py
+++ cl_spider/app/__init__.py
@@ -1,20 +1,15 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_babelex import Babel
-from flask_bootstrap import Bootstrap
+from concurrent.futures import ThreadPoolExecutor
 
+from flask import Flask
+from flask_babel import Babel
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+
+executor = ThreadPoolExecutor(max_workers=2)
 # create application
 app = Flask(__name__, template_folder='templates')
 
 app.config.from_pyfile('config.py')
-
-app.config['DATABASE_FILE'] = 'db.sqlite'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-    app.config['DATABASE_FILE']
-app.config['SQLALCHEMY_ECHO'] = True
-
-app.config['SECRET_KEY'] = 'very very hard to guess token'
-app.config['BABEL_DEFAULT_LOCALE'] = 'zh_CN'
 
 db = SQLAlchemy(app)
 babel = Babel(app)
