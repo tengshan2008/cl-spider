8f4e39f0953d30c7cb8823b998bebf0df482186d
tengshan2008 <tengshan2008@hotmail.com>
2021-05-04 14:40:41 +0800

    UPDATE format and thread method

--- cl_spider/app/main.py
+++ cl_spider/app/main.py
@@ -1,20 +1,39 @@
-from flask import url_for, redirect
+from datetime import date
+
+from cl_spider.app import app, db, executor
+from cl_spider.app.models import Novel, Picture
+from flask import redirect
 from flask.helpers import flash
 from flask_admin import Admin, BaseView, expose
+from flask_admin.base import AdminIndexView
 from flask_admin.contrib.sqla import ModelView
-
+from flask_admin.model import typefmt
 from flask_wtf import FlaskForm
+from markupsafe import Markup
 from wtforms.fields.html5 import URLField
 from wtforms.fields.simple import SubmitField
 from wtforms.validators import DataRequired
-from cl_spider.app import app, db
-from cl_spider.app.models import Novel, Picture
 
 
 # Flask views
 @app.route('/')
 def index():
     return redirect('/admin')
+
+
+def share_link_formatter(view, context, model, name):
+    field = getattr(model, name)
+    return Markup(f'<a href="{field}">LINK</a>')
+
+
+def date_format(view, value):
+    return value.strftime('%Y-%m-%d %H:%M:%S')
+
+
+MY_DEFAULT_FORMATTERS = dict(typefmt.BASE_FORMATTERS)
+MY_DEFAULT_FORMATTERS.update({
+    date: date_format,
+})
 
 
 # customized admin interface
@@ -26,13 +45,13 @@ class NovelAdmin(CustomView):
     column_searchable_list = ('title', )
     column_labels = {
         'origin_id': 'ID',
-        'title': '标题',
-        'author': '作者',
-        'public_datetime': '发布日期',
-        'category': '类别',
-        'size': '字数',
-        'created_at': '创建日期',
-        'updated_at': '更新日期'
+        'title': u'标题',
+        'author': u'作者',
+        'public_datetime': u'发布日期',
+        'category': u'类别',
+        'size': u'字数',
+        'created_at': u'创建日期',
+        'updated_at': u'更新日期',
     }
     column_list = list(column_labels.keys())
     # column_details_list = ('path')
@@ -44,14 +63,21 @@ class NovelAdmin(CustomView):
 
 
 class PictureAdmin(CustomView):
+    column_type_formatters = MY_DEFAULT_FORMATTERS
     column_searchable_list = ('title', )
     column_labels = {
-        'origin_id': 'ID',
-        'title': '标题',
-        'author': '作者',
-        'public_datetime': '发布日期',
+        'id': 'ID',
+        'origin_id': 'TID',
+        'title': u'标题',
+        'author': u'作者',
+        'public_datetime': u'发布日期',
+        'share': u'预览',
+        'updated_at': u'更新日期',
     }
+    column_formatters = {'share': share_link_formatter}
     column_list = list(column_labels.keys())
+    column_default_sort = ('updated_at', True)
+    can_set_page_size = True
     can_create, can_delete, can_edit = False, False, False
 
     def __init__(self, session, **kwargs):
@@ -74,29 +100,37 @@ class PictureTaskForm(FlaskForm):
 
 class PictureTaskView(BaseView):
     @expose('/', methods=['GET', 'POST'])
+    # @run_async
+    # async def index(self):
     def index(self):
         from cl_spider.spiders.picture import PictureSpider
 
         form = PictureTaskForm()
         if form.validate_on_submit():
             if form.url:
-                spider = PictureSpider.load(set([form.url]))
-                # spider.get_latest()
-                flash("提交成功")
+                spider = PictureSpider.load(set(form.url.raw_data))
+                executor.submit(spider.get_latest)
+                flash(u'提交成功')
                 return redirect('/admin/picture')
             else:
-                flash("校验错误")
+                flash(u'校验错误')
 
         return self.render('picture_task.html', form=form)
 
 
 # create admin with custom base template
-admin = Admin(app, 'CL-Spider', template_mode='bootstrap4')
+index_view = AdminIndexView(name=u'导航栏', template='index.html', url='/admin')
+admin = Admin(
+    app,
+    'CL-Spider',
+    index_view=index_view,
+    template_mode='bootstrap4',
+)
 
-admin.add_view(NovelAdmin(db.session, name='小说'))
-admin.add_view(PictureAdmin(db.session, name='图片'))
-admin.add_view(NovelTaskView(name='小说', category='新建'))
-admin.add_view(PictureTaskView(name='图片', category='新建'))
+admin.add_view(NovelAdmin(db.session, name=u'小说'))
+admin.add_view(PictureAdmin(db.session, name=u'图片'))
+admin.add_view(NovelTaskView(name=u'小说', category=u'新建'))
+admin.add_view(PictureTaskView(name=u'图片', category=u'新建'))
 
 
 def init_db():
