28bdcda70bbf07bc9a7e9a170adeb9db17e61316
tengshan2008 <tengshan2008@hotmail.com>
2021-05-05 20:23:55 +0800

    UPDATE novel test

---
+++ tests/test_novel.py
@@ -0,0 +1,40 @@
+import unittest
+from cl_spider.spiders.novel import NovelSpider
+
+TEST_SINGLE_PAGE = 'https://cl.192x.xyz/htm_data/2105/20/4476662.html'
+TEST_MULTIPLE_PAGE = 'https://cl.192x.xyz/htm_data/2104/20/4456719.html'
+
+
+class NovelSpiderTest(unittest.TestCase):
+    def test_load_data(self):
+        ns = NovelSpider()
+        data = ns.load_data(TEST_SINGLE_PAGE)
+        print(data)
+
+    def test_single_page_get_pages(self):
+        ns = NovelSpider()
+        pages = ns.get_pages(TEST_SINGLE_PAGE)
+        print(pages)
+
+    def test_multiple_page_get_pages(self):
+        ns = NovelSpider()
+        pages = ns.get_pages(TEST_MULTIPLE_PAGE)
+        print(pages)
+
+    def test_single_page_parse_data(self):
+        url = TEST_SINGLE_PAGE
+        ns = NovelSpider()
+        data = ns.load_data(url)
+        parsed_data = ns.parse_data(url, data)
+        print(parsed_data)
+
+    def test_multiple_page_parse_data(self):
+        url = TEST_MULTIPLE_PAGE
+        ns = NovelSpider()
+        data = ns.load_data(url)
+        parsed_data = ns.parse_data(url, data)
+        print(parsed_data)
+
+    def test_get_latest(self):
+        ns = NovelSpider()
+        ns.get_latest(TEST_MULTIPLE_PAGE)
