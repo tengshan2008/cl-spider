36d02dc9f580e3bd96f0e67fdd2677dd4ce52a5e
tengshan2008 <tengshan2008@hotmail.com>
2021-05-05 20:23:41 +0800

    UPDATE single novel crawl

--- cl_spider/spiders/novel.py
+++ cl_spider/spiders/novel.py
@@ -1,76 +1,237 @@
-from typing import Any, Dict, List, Text
+from io import BytesIO
+from typing import Any, Dict, List, Optional, Text, Tuple
 
+import dateutil.parser
+from bs4 import BeautifulSoup
 from cl_spider.app.models import Novel, db
+from cl_spider.config import NOVEL_BUCKET_NAME
+from cl_spider.spiders.file_uploader import Uploader
 from cl_spider.spiders.manager import Manager
 from cl_spider.spiders.spider import Spider
+from loguru import logger
+import urllib.parse
+
+TID_KEY = 'tid'
+TITLE_KEY = 'title'
+CATEGORY_KEY = 'category'
+AUTHOR_KEY = 'author'
+PUBLIC_DATETIME_KEY = 'public_datetime'
+CONTENT_KEY = 'content'
+CATEGORIES = [
+    '[現代奇幻]', '[古典武俠]', '[另類禁忌]', '[性愛技巧]', '[笑話連篇]', '[有声小说]', '[其他交流]'
+]
+
+
+class IndexManager(Manager):
+    def __init__(self, limit: Optional[int] = None) -> None:
+        super().__init__(limit=limit)
 
 
 class NovelManager(Manager):
-    def __init__(self) -> None:
-        super().__init__()
-
-    def get_new_novel(self):
-        new_novel = self.queue.popleft()
-        self.visited.add(new_novel)
-        return new_novel
-
-    def add_new_novel(self, novel: Novel):
-        if novel is None:
-            return False
-        if novel not in self.queue and novel not in self.visited:
-            self.queue.append(novel)
-
-    def add_new_novels(self, novels: List[Novel]):
-        if novels is None or len(novels) == 0:
-            return
-        for novel in novels:
-            self.add_new_url(novel)
+    def __init__(self, limit: Optional[int] = None) -> None:
+        super().__init__(limit=limit)
 
 
 class IndexSpider(Spider):
-    def __init__(self, headers: Dict[Text, Text] = None) -> None:
+    def __init__(
+        self,
+        headers: Dict[Text, Text] = None,
+        manager: Optional[IndexManager] = None,
+        limit: Optional[int] = None,
+    ) -> None:
         super().__init__(headers=headers)
+        self.manager = manager if manager else IndexManager(limit=limit)
 
-    def get_index_data(self):
+    def load_data(self, url: Text):
         pass
 
-    def parse_html(self):
+    def parse_data(self, url: Text, data: BeautifulSoup):
         pass
 
-    def exec_database(self):
+    def save_data(self, url: Text, parsed_data: Dict, metadata: Dict):
         pass
+
+    def get_latest(self, metadata: Optional[Dict[Text, Any]] = None) -> None:
+        while self.manager.has_new_url():
+            self.random_sleep
+            url = self.manager.get_new_url()
+            data = self.load_data(url)
+            parsed_data = self.parse_data(url, data)
+            self.save_data(url, parsed_data, metadata)
+
 
 class NovelSpider(Spider):
-    def __init__(self, headers: Dict[Text, Text] = None) -> None:
+    def __init__(
+        self,
+        headers: Dict[Text, Text] = None,
+        manager: Optional[NovelManager] = None,
+        novel_info: Optional[Dict[Text, Any]] = None,
+        limit: Optional[int] = None,
+    ) -> None:
         super().__init__(headers=headers)
+        self.parsed_data = novel_info.copy() if novel_info else {
+            CONTENT_KEY: {}
+        }
+        self.manager = manager if manager else NovelManager(limit=limit)
 
-    def get_book_data(self, chapter_html):
-        data = {}  # 小说的元信息
-        return data
+    def load_data(self, url: Text) -> BeautifulSoup:
+        logger.info(f'route is "{self.format_url(url)}", have loaded.')
+        return self.open(url)
 
-    @staticmethod
-    def parse_html(content: Text) -> Dict[Text, Any]:
-        return {}
+    def parse_tid(self, url: Text) -> Text:
+        r = urllib.parse.urlsplit(url)
+        return urllib.parse.parse_qs(r.query)[TID_KEY][0]
 
-    @staticmethod
-    def exec_database(data: Dict) -> None:
-        novel = Novel(
-            origin_id=data["origin_id"],
-            title=data["title"],
-            author=data["author"],
-        )
-        if Novel.query.filter_by(origin_id=data["origin_id"]).first() is None:
-            db.session.add(novel)
+    def parse_title_category(self, data: BeautifulSoup) -> Tuple[Text, Text]:
+        title = data.head.title.string.strip()
+        title = title.replace('技術討論區', '')
+        title = title.replace('草榴社區', '')
+        title = title.replace('t66y.com', '')
+        title = title.replace('成人文學交流區', '')
+        title = self.format_string(title)
+
+        for cate in CATEGORIES:
+            if cate in title:
+                return title.replace(cate, ''), cate[1:-1]
+        return title, '[UNKNOW]'
+
+    def parse_public_datetime(self, data: BeautifulSoup) -> Text:
+        tipad = data.find('div', attrs={'class': 'tipad'}).stripped_strings
+        return dateutil.parser.parse(list(tipad)[1].replace('Posted:', ''))
+
+    def parse_author(self, data: BeautifulSoup) -> Text:
+        users = data.find_all(name='div', attrs={'class': 'tpc_detail f10 fl'})
+        names = data.find_all(name='b')
+
+        if len(users) > 0:
+            return list(users[0].strings)[0]
+        elif len(names) > 0:
+            return names[3].string
         else:
-            old_novel = Novel.query.filter_by(origin_id=data["origin_id"]).first()
-            novel.created_at = old_novel.created_at
-            db.session.update(novel)
+            logger.warning("not find author")
+            return "unknow"
 
-    def get_latest_novel(self):
-        # 获取网页信息
-        content = ""
-        # 解析网页信息
-        data = self.parse_html(content)
-        # 保留有效信息
-        self.exec_database(data)
-        return None
+    def parse_content(self, data: BeautifulSoup) -> Text:
+        cells = data.find_all(name='div', attrs={'class': 'tpc_content'})
+        content = []
+        for cell in cells:
+            content += list(cell.stripped_strings)
+        return '\n'.join(content)
+
+    def parse_data(self, url: Text, data: BeautifulSoup) -> Dict[Text, Any]:
+        if data is None:
+            logger.warning(f'route is "{self.format_url(url)}", load failed.')
+            self.manager.add_new_url(url)
+            return None
+
+        if 'page' not in url:
+            if TID_KEY not in self.parsed_data:
+                self.parsed_data[TID_KEY] = self.parse_tid(url)
+            if (TITLE_KEY
+                    not in self.parsed_data) or (CATEGORY_KEY
+                                                 not in self.parsed_data):
+                title, category = self.parse_title_category(data)
+                self.parsed_data[TITLE_KEY] = title
+                self.parsed_data[CATEGORY_KEY] = category
+            if PUBLIC_DATETIME_KEY not in self.parsed_data:
+                self.parsed_data[
+                    PUBLIC_DATETIME_KEY] = self.parse_public_datetime(data)
+            if AUTHOR_KEY not in self.parsed_data:
+                self.parsed_data[AUTHOR_KEY] = self.parse_author(data)
+
+        self.parsed_data[CONTENT_KEY][url] = self.parse_content(data)
+
+    def exec_minio(
+        self,
+        uploader: Uploader,
+        bucket_name: Text,
+        object_name: Text,
+        data: bytes,
+    ) -> Text:
+        uploader.put_object_with_file_type(bucket_name,
+                                           object_name,
+                                           BytesIO(data),
+                                           file_type='txt')
+        share = uploader.get_object_share(bucket_name, object_name)
+        return share
+
+    def exec_database(self, link: Text, share: Text, size: int) -> None:
+        novel = Novel(
+            origin_id=self.parsed_data[TID_KEY],
+            title=self.parsed_data[TITLE_KEY],
+            author=self.parsed_data[AUTHOR_KEY],
+            public_datetime=self.parsed_data[PUBLIC_DATETIME_KEY],
+            category=self.parsed_data[CATEGORY_KEY],
+            link=link,
+            size=str(size),
+            share=share,
+        )
+        db.session.add(novel)
+        db.session.commit()
+
+    def merge_content(self, pages: List[Text]) -> Text:
+        content = []
+        for page in pages:
+            if page in self.parsed_data[CONTENT_KEY]:
+                content.append(self.parsed_data[CONTENT_KEY][page])
+
+        return '\n'.join(content)
+
+    def is_need_put_or_update(self):
+        return True
+
+    def save_data(
+        self,
+        pages: List[Text],
+        metadata: Optional[Dict[Text, Any]] = None,
+    ) -> None:
+        if self.parsed_data is None:
+            logger.warning(
+                f'route is "{self.format_url(pages[0])}", parse failed.')
+            return None
+
+        bucket_name = NOVEL_BUCKET_NAME
+        uploader = Uploader(metadata)
+        uploader.create_bucket(bucket_name)
+
+        content = self.merge_content(pages)
+        if self.is_need_put_or_update():
+            pub_date = self.parsed_data[PUBLIC_DATETIME_KEY].strftime('%Y-%m')
+            object_name = f"{pub_date}/{self.parsed_data[TITLE_KEY]}.txt"
+            share = self.exec_minio(uploader, bucket_name, object_name,
+                                    content.encode(encoding="utf-8"))
+            if share:
+                self.exec_database(pages[0], share, len(content))
+
+    def get_pages(self, url: Text) -> List[Text]:
+        only_author_url = (f'{self.format_url_host(url)}/read.php'
+                           f'?tid={self.parse_tid(url)}&toread=2')
+        data = self.load_data(only_author_url)
+
+        div_pages = data.find_all(name='div', attrs={'class': 'pages'})
+        if len(div_pages) < 2:
+            return [url]
+
+        page_range = div_pages[0].find(name='a', attrs={
+            'class': 'w70'
+        }).input.get('value').split('/')
+
+        return [only_author_url] + [
+            f'{self.format_url_host(url)}/read.php'
+            f'?tid={self.parse_tid(url)}&toread=2&page={i}'
+            for i in range(2,
+                           int(page_range[-1]) + 1)
+        ]
+
+    def get_latest(self,
+                   url: Text,
+                   metadata: Optional[Dict[Text, Any]] = None) -> None:
+        pages = self.get_pages(url)
+        self.manager.add_new_urls(set(pages))
+
+        while self.manager.has_new_url():
+            self.random_sleep
+            url = self.manager.get_new_url()
+            data = self.load_data(url)
+            self.parse_data(url, data)
+        self.save_data(pages, metadata)
