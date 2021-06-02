5421887d6fb2f796f3b58ac851e6383c5a11e1bd
tengshan2008 <tengshan2008@hotmail.com>
2021-06-02 21:41:41 +0800

    UPDATE requirement

--- cl_spider/app/__init__.py
+++ cl_spider/app/__init__.py
@@ -10,6 +10,7 @@ logger.add('logs/server.log', enqueue=True)
 
 executor = ThreadPoolExecutor(max_workers=4)
 
+
 # create application
 def create_app():
     app = Flask(__name__, template_folder='templates')
@@ -17,8 +18,8 @@ def create_app():
 
     return app
 
+
 app = create_app()
 db = SQLAlchemy(app)
 babel = Babel(app)
 bootstrap = Bootstrap(app)
-
