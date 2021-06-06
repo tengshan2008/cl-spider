6f34cf5e92df0737ba6ff8dddb89f5f7e9b3b900
tengshan2008 <tengshan2008@hotmail.com>
2021-06-06 18:28:26 +0800

    UPDATE minio config

--- cl_spider/config.py
+++ cl_spider/config.py
@@ -1,5 +1,5 @@
 # minio server
-MINIO_ENDPOINT = "server:9090"
+MINIO_ENDPOINT = "minio:9000"
 # MINIO_ENDPOINT = "192.168.1.5:9090"
 MINIO_ACCESS_KEY = "admin"
 MINIO_SECRET_KEY = "8rO8qpl3VASXagqX"
